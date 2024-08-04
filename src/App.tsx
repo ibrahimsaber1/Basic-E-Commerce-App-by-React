c120af76b19744b8a7f141d74d0e49e76e429fb7
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-04 17:34:37 +0300

    src: Add products.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // import { useState } from 'react'
 import Navbar from './components/Navbar';
-
 import Slider from './components/Slider';
+import Products from './components/Products';
 function App() {
 
   return (
     <>
       <Navbar />
       <Slider />
+      <Products />
     </>
   )
 }
