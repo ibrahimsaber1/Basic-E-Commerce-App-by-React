0fda5493c47971f7d533288b3c6a09d529c9a163
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-04 19:57:00 +0300

    maintsx: install react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter } from "react-router-dom";
 // import './index.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,6 +9,9 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+    
   </React.StrictMode>,
 )
