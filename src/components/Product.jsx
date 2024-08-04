00ff9dc406e10f6c6fddf7e934633f7bc1b93a50
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-04 19:43:45 +0300

    products : feach the data

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,25 @@
-function Product(){
-    return(
-        <>
-        
-        </>
-    )
-}
+function Product(props) {
+  console.log(props); // Corrected console log
 
+  return (
+    <>
+      <div className="card">
+        <img
+          src={props.product.images[0]} 
+          className="card-img-top" 
+          alt={props.product.title} 
+
+          height={200}
+        />
+        <div className="card-body">
+          <h4>{props.product.title}</h4>
+          <h4>${props.product.price.toFixed(2)}</h4> {/* Formatting price */}
+          <p>{props.product.description}</p>
+          <button className="btn btn-primary">Go somewhere</button>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Product;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,27 +1,31 @@
-function Products(){
-    return(
+import { useEffect, useState } from "react";
+import Product from "./Product";
+
+function Products() {
+  const api_url = "https://dummyjson.com/products";
+  
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(api_url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  }, []);
+
+  return (
     <>
-    <h2 className="text-center p-2">Our Products</h2>
-    
-    <div className="container">
-      <div className="row">
-        <div className="col-3">
-          <div class="card" >
-              <img src="..." class="card-img-top" alt="..."/>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button class="btn btn-primary">Go somewhere</button>
-              </div>
-          </div>
-        </div>
-      </div> {/* row  */}
-
-
-    </div> {/*  container*/}
+      <h2 className="text-center p-2">Our Products</h2>
+      <div className="container">
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-3" key={product.id}>
+              <Product product={product} />
+            </div>
+          ))}
+        </div> {/* row */}
+      </div> {/* container */}
     </>
-    )
+  );
 }
-
 
 export default Products;
