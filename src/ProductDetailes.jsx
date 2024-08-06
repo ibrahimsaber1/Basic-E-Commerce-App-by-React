6ccc851645b4e85f1fd36b6678d901bfe19dbc51
IBRAHIM SABER <ibrahimsamosee@gmail.com>
2024-08-06 06:17:48 +0300

    productditalies: fech single product data

--- src/ProductDetailes.jsx
+++ src/ProductDetailes.jsx
@@ -1,12 +1,20 @@
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function ProductDetailes (){
-    const params = useParams()
-    console.log(params)
+function ProductDetails() {
+    const params = useParams();
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/products/${params.productId}`)
+            .then((res) => res.json())
+            .then((product) => setProduct(product))
+            .catch((error) => console.error("Error fetching product:", error));
+    }, [params.productId]);
+
     return (
-        <h1>Product Detalies</h1>
-    )
-
+        <h1>Product Details: {product.title} (ID: {params.productId})</h1>
+    );
 }
 
-export default ProductDetailes 
+export default ProductDetails;
